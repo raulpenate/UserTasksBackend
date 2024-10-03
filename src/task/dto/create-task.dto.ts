64dcb259df41557d5714643b1f070f9ceb4c61c5
raulpenate <50472064+raulpenate@users.noreply.github.com>
2024-10-03 09:43:54 -0600

    ✨ Adding class validators
    for safeness

--- src/task/dto/create-task.dto.ts
+++ src/task/dto/create-task.dto.ts
@@ -1,4 +1,14 @@
+import { IsString, MinLength } from "class-validator";
+
 export class CreateTaskDto {
+    @IsString()
+    @MinLength(2)
+    title: string;
+
+    @IsString()
+    @MinLength(2)
     description: string;
+
+    
     userId: number;
   }
--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,5 +1,15 @@
+import { IsInt, IsPositive, IsString, MinLength } from "class-validator";
+
 export class CreateUserDto {
+    @IsString()
+    @MinLength(2)
     firstName: string;
+
+    @IsString()
+    @MinLength(2)
     lastName: string;
+
+    @IsInt()
+    @IsPositive()
     age: number;
 }
